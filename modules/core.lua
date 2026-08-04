af374a6f5e83bc46ca680ae42115745e78f6a141
nyull <0utof0ptions@proton.me>
2026-08-04 16:57:41 +0800

    added values for core

---
+++ modules/core.lua
@@ -0,0 +1,10 @@
+hl.config({
+  general = {
+    border_size = 1,
+    gaps_in = 2,
+    gaps_out = 3,
+    col.inactive_border = 0xff444444, --def
+    col.active_border = 0xffffffff,--def
+    resize_on_border = true,
+  }
+})
