facf76f23353b778239b60efa84888785b03b3c1
nyull <0utof0ptions@proton.me>
2026-08-06 23:38:02 +0800

    fixed some issues

--- hypr/modules/core.lua
+++ hypr/modules/core.lua
@@ -1,10 +1,13 @@
 hl.config({
-  general = {
-    border_size = 1,
-    gaps_in = 2,
-    gaps_out = 3,
-    col.inactive_border = 0xff444444, --def
-    col.active_border = 0xffffffff,--def
-    resize_on_border = true,
-  }
+	general = {
+		border_size = 1,
+		gaps_in = 2,
+		gaps_out = 3,
+		col = {
+			inactive_border = 0xff444444, --def
+			active_border = 0xffffffff, --def
+		},
+		layout = "dwindle",
+		resize_on_border = true,
+	},
 })
--- hypr/modules/keybinds.lua
+++ hypr/modules/keybinds.lua
@@ -1,33 +1,35 @@
 local filerer = "thunar"
 local terminal = "kitty"
-local menu = "wofi --show drun"
+local menu = "rofi -show drun"
 hl.bind("SUPER + E", hl.dsp.exec_cmd(filerer))
-hl.bind("SUPER + ENTER", hl.dsp.exec_cmd(terminal))
+hl.bind("SUPER + RETURN", hl.dsp.exec_cmd(terminal))
 hl.bind("SUPER + R", hl.dsp.exec_cmd(menu))
 
-hl.bind("SUPER + Q", hl.dsp.window.close({}))
+hl.bind("SUPER + Q", hl.dsp.window.close())
 
-hl.bind("SUPER + K", hl.dsp.focus({ direction = u }))
+--TODO: Fix whatever annoying shit is happening here
+--[[hl.bind("SUPER + K", hl.dsp.focus({ direction = u }))
 hl.bind("SUPER + J", hl.dsp.focus({ direction = d }))
 hl.bind("SUPER + H", hl.dsp.focus({ direction = r }))
-hl.bind("SUPER + L", hl.dsp.focus({ direction = l }))
+hl.bind("SUPER + L", hl.dsp.focus({ direction = l })) ]]
+--
 for i = 1, 10 do -- automates workshop shifting from wrkshp 1 to 10
 	local key = (i % 10)
-
+	hl.bind("SUPER + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 	hl.bind("SUPER +" .. key, function()
-		hl.dispatch(hl.dsp.window.move({ workspace = key }))
+		hl.dispatch(hl.dsp.window.move({ workspace = i }))
 	end)
 end
 
-hl.bind("mouse:272", hl.dsp.resize())
-hl.bind("SUPER + mouse:272", hl.dsp.drag())
-hl.bind("SUPER + SHIFT + K", hl.dsp.resize({ y = 20 }))
-hl.bind("SUPER + SHIFT + J", hl.dsp.resize({ y = -20 }))
-hl.bind("SUPER + SHIFT + H", hl.dsp.resize({ x = -5 }))
-hl.bind("SUPER + SHIFT + L", hl.dsp.resize({ x = 5 }))
+hl.bind("mouse:272", hl.dsp.window.resize())
+hl.bind("SUPER + mouse:272", hl.dsp.window.drag())
+hl.bind("SUPER + SHIFT + K", hl.dsp.window.resize({ x = 0, y = 20, relative, true }))
+hl.bind("SUPER + SHIFT + J", hl.dsp.window.resize({ x = 0, y = -20, relative, true }))
+hl.bind("SUPER + SHIFT + H", hl.dsp.window.resize({ x = -5, y = 0, relative, true }))
+hl.bind("SUPER + SHIFT + L", hl.dsp.window.resize({ x = 5, y = 0, relative, true }))
 hl.bind("SUPER + V", hl.dsp.window.float({}))
 
-hl.bind("", hl.dsp) --boilerplate, for copypasting
+--hl.bind("", hl.dsp)
 
 hl.bind("SUPER + F", hl.dsp.window.fullscreen({ mode = maximized }))
 hl.bind("SUPER + F", hl.dsp.window.fullscreen({ mode = fullscreen }))
