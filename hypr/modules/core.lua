5d72bcbe15da8ed33e65467b99e633a414c2e05f
nyull <0utof0ptions@proton.me>
2026-08-09 14:00:31 +0800

    added easing styles

--- hypr/modules/core.lua
+++ hypr/modules/core.lua
@@ -18,6 +18,13 @@ hl.config({
 		active_opacity = 1,
 		inactive_opacity = 0.70,
 	},
-
-	animation = {}, -- TODO: color change when switching active border
 })
+
+hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
+hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
+hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
+hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
+hl.curve("easy", { type = "spring", mass = 1, stiffness = 238.1191, dampening = 24.21279333 })
+
+hl.animation({ leaf = "border", enabled = true, speed = 2, bezier = "easeInOutCubic" })
